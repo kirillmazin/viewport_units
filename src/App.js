013188ec20b72bf00ab17673f2cdf47f59efb20e
Kirill Mazin <kmazin@gmail.com>
2020-01-29 17:40:55 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h3>Getting started.</h3>
+       <ul>
+         <li>What is HiRoad?</li>
+         <li>How does the HiRoad app work?</li>
+         <li>What hardware and software do I need?</li>
+         <li>What are driving scores in the app?</li>
+         <li>How does billing work?</li>
+       </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
